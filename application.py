bcfecc32d92b183a065f871626ffa6e5ccfcf8e1
Quinn Le <quynhnle135@gmail.com>
2022-12-07 11:16:09 -0500

    Learned about data types in Python

--- application.py
+++ application.py
@@ -1,23 +1,28 @@
 # Variables
-print('Hello World')
+# print('Hello World')
+#
+# print('This is the next statement')
+# number = 8852
+#
+# print('number')
+# print(number)
+#
+# number = 8854
+# print(number)
+#
+# weight = 100
+# answer = number + weight
+# print(answer)
+#
+# first_name = 'Quinn'
+# last_name = 'Le'
+#
+# name = first_name + ' ' + last_name
+# print('My name is ', name)
+# sentence = first_name + last_name + " weights " + str(weight) + " lbs"
+# print(sentence)
 
-print('This is the next statement')
-number = 8852
-
-print('number')
-print(number)
-
-number = 8854
-print(number)
-
-weight = 100
-answer = number + weight
-print(answer)
-
-first_name = 'Quinn'
-last_name = 'Le'
-
-name = first_name + ' ' + last_name
-print('My name is ', name)
-sentence = first_name + last_name + " weights " + str(weight) + " lbs"
-print(sentence)
+# Data types
+data_type = 12
+print(data_type)
+print(type(data_type))
