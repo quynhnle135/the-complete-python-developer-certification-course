88e171d01199084969caf46fbb7c9dcdbb90f95a
Quinn Le <quynhnle135@gmail.com>
2022-12-07 11:08:55 -0500

    Learned about variables and how to concatinate string in Python

---
+++ application.py
@@ -0,0 +1,23 @@
+# Variables
+print('Hello World')
+
+print('This is the next statement')
+number = 8852
+
+print('number')
+print(number)
+
+number = 8854
+print(number)
+
+weight = 100
+answer = number + weight
+print(answer)
+
+first_name = 'Quinn'
+last_name = 'Le'
+
+name = first_name + ' ' + last_name
+print('My name is ', name)
+sentence = first_name + last_name + " weights " + str(weight) + " lbs"
+print(sentence)
