ee0da726b7d8b6a6b60bf3034f4446e95e6915c4
Quinn Le <quynhnle135@gmail.com>
2022-12-09 10:44:27 -0500

    Learned about function in Python

---
+++ functions.py
@@ -0,0 +1,24 @@
+# def greet_person(name='your name'):
+#     '''
+#     DOCSTRING: this returns a greeting
+#     INPUT: name
+#     OUTPUT: hello...name
+#     '''
+#
+#     print('this is all body of a function')
+#     print('hey there')
+#     print('hello ' + name + " this is a greeting...")
+
+# greet_person('Quinn')
+# greet_person()
+
+def remainder(num1, num2):
+    """
+    INPUT: num1, num2 - these are the numbers to divide to get a remainder with %
+
+    """
+    return num1 % num2
+
+print(remainder(10, 3))
+
+
